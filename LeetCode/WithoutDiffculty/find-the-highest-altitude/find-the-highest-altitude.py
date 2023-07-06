1a58113de807822fefad22d692e20edc4b2bbb63
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-07-06 13:38:52 +0900

    Delete LeetCode/WithoutDiffculty directory

--- LeetCode/WithoutDiffculty/find-the-highest-altitude/find-the-highest-altitude.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def largestAltitude(self, gain: List[int]) -> int:
-        sum = 0
-        max = 0
-        for i in gain:
-            if sum + i >= max:
-                max = sum + i
-            sum += i
-        return max
