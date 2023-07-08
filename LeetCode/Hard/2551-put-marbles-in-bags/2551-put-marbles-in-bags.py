aec02b229f2ab41a44b25ac6c56588782fb0ef94
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-07-08 13:06:43 +0900

    Time: 713 ms (88.5%), Space: 30 MB (55.75%) - LeetHub

---
+++ LeetCode/Hard/2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -0,0 +1,14 @@
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        n = len(weights)
+        if n == k:
+            return 0
+        bag = [0] * (n-1)
+        for i in range(n-1):
+            bag[i] = weights[i] + weights[i+1]
+        bag.sort()
+
+        max_fair = sum(bag[n-k:])
+        min_fair = sum(bag[:k-1])
+
+        return max_fair - min_fair
