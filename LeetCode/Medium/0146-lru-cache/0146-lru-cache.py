f982325ed5ef1b14bf9bd573be892bdd8784fb7b
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-07-18 14:33:15 +0900

    Time: 2751 ms (5.16%), Space: 77.9 MB (73.01%) - LeetHub

--- LeetCode/Medium/0146-lru-cache/0146-lru-cache.py
+++ LeetCode/Medium/0146-lru-cache/0146-lru-cache.py
@@ -13,21 +13,14 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int) -> None:
-        # key가 존재할 때
         if key in self.cache:
             self.lru_keys.remove(key)
-            self.lru_keys.append(key)
-            self.cache[key] = value
-        # key가 존재하지 않을 때
-        else:
-            if len(self.cache) >= self.capacity:
-                last_key = self.lru_keys.pop(0)
-                del self.cache[last_key]
-                self.cache[key] = value
-                self.lru_keys.append(key)
-            else:
-                self.cache[key] = value
-                self.lru_keys.append(key)
+        elif len(self.cache) >= self.capacity:
+            last_key = self.lru_keys.pop(0)
+            del self.cache[last_key]
+
+        self.cache[key] = value
+        self.lru_keys.append(key)
             
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
