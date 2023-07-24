8b2e7b75b6ceb77c776de6183f12268ecd758855
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-07-24 10:00:21 +0900

    Time: 54 ms (13.52%), Space: 16.5 MB (7.14%) - LeetHub

--- LeetCode/Medium/0050-powx-n/0050-powx-n.py
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.py
@@ -1,7 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        elif n < 0:
-            return pow(1 / x, -n)
-        return pow(x, n)
+        @functools.lru_cache(None)
+        def solve(x, n):
+            if n == 0:
+                return 1
+            # even
+            elif n % 2 == 0:
+                return solve(x * x, n /2)
+            # odd
+            else:
+                return x * solve(x * x, (n-1)/2)
+        return solve(x,n) if n >= 0 else 1/solve(x,-n)
