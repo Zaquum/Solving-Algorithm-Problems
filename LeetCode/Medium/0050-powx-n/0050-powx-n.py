f8ae69c2c63aabde0e8faa39c9453dfeae3ed4fa
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-07-24 10:01:19 +0900

    Time: 55 ms (10.92%), Space: 16.5 MB (7.14%) - LeetHub

--- LeetCode/Medium/0050-powx-n/0050-powx-n.py
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.py
@@ -1,5 +1,6 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+
         @functools.lru_cache(None)
         def solve(x, n):
             if n == 0:
@@ -10,4 +11,5 @@ class Solution:
             # odd
             else:
                 return x * solve(x * x, (n-1)/2)
+                
         return solve(x,n) if n >= 0 else 1/solve(x,-n)
