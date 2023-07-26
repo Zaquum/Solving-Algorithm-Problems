e0bdb671c5ce207ac26c497827d5524129e58c6b
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-07-26 12:12:06 +0900

    Time: 3427 ms (44.73%), Space: 30.5 MB (75.78%) - LeetHub

---
+++ LeetCode/Medium/1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        def possible(speed):
+            time = sum(ceil(d/speed) for d in dist[:-1])
+            time += dist[-1] / speed
+            if time > hour:
+                return False
+            return time
+
+        if len(dist) - 1 >= hour:
+            return -1
+        
+        left,right = 1, 10**7 * max(dist)
+        while left < right:
+            mid = (left+right) // 2
+            if possible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+        # O(n)
+        # if len(dist) - 1 >= hour:
+        #     return -1
+        
+        # speed = math.ceil(sum(dist) / hour) # Start from this speed
+
+        # while True:
+        #     total = 0.0
+        #     for i in range(len(dist)):
+        #         spend = dist[i] / speed
+        #         if i != len(dist) - 1:  # if not the last train, round up to nearest hour
+        #             spend = math.ceil(spend)
+        #         total += spend
+        #     if total <= hour:  # If we can reach in time, return speed
+        #         return speed if speed <= 10**7 else -1
+        #     speed += 1  # else, increase the speed
