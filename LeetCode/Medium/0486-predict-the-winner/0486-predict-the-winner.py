f517659083f455d8e6d08a1c76f71e1887123872
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-07-28 17:04:46 +0900

    Time: 42 ms (95.15%), Space: 16.7 MB (7.89%) - LeetHub

--- LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.py
+++ LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,23 +1,38 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         n = len(nums)
-        player_sum = 0
-        def solve(nums: List[int], start, end, player_sum, turn):
-            if start > end:
-                if player_sum >= 0:
-                    return True
-                else:
-                    return False
+        memo = {}
+        
+        def dp(start, end):
+            if (start, end) in memo:
+                return memo[(start, end)]
+            
+            if start == end:
+                return nums[start]
+            
+            memo[(start, end)] = max(nums[start] - dp(start + 1, end), nums[end] - dp(start, end - 1))
+            
+            return memo[(start, end)]
+        
+        return dp(0, n-1) >= 0
+        # n = len(nums)
+        # player_sum = 0
+        # def solve(nums: List[int], start, end, player_sum, turn):
+        #     if start > end:
+        #         if player_sum >= 0:
+        #             return True
+        #         else:
+        #             return False
 
-            first = nums[start]
-            last = nums[end]
-            # player1 turn
-            if turn == 0:
-                return (solve(nums, start+1, end, player_sum + first, 1) or
-                        solve(nums, start, end-1, player_sum + last, 1))
-            # player2 turn
-            elif turn == 1:
-                return (solve(nums, start+1, end, player_sum - first, 0) and
-                        solve(nums, start, end-1, player_sum - last, 0))
+        #     first = nums[start]
+        #     last = nums[end]
+        #     # player1 turn
+        #     if turn == 0:
+        #         return (solve(nums, start+1, end, player_sum + first, 1) or
+        #                 solve(nums, start, end-1, player_sum + last, 1))
+        #     # player2 turn
+        #     elif turn == 1:
+        #         return (solve(nums, start+1, end, player_sum - first, 0) and
+        #                 solve(nums, start, end-1, player_sum - last, 0))
 
-        return solve(nums, 0, n-1, 0, 0)
+        # return solve(nums, 0, n-1, 0, 0)
