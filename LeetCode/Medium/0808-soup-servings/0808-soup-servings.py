0661d58382ffc9b6f174f88f473635af432a5d29
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-07-29 14:00:12 +0900

    Time: 42 ms (97.17%), Space: 17 MB (67.92%) - LeetHub

---
+++ LeetCode/Medium/0808-soup-servings/0808-soup-servings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n > 4800:
+            return 1.0
+        operations = [(100,0),(75,25),(50,50),(25,75)]
+        memo = {}
+
+        def solve(soupA, soupB):
+            # return condition
+            if (soupA, soupB) in memo:
+                return memo[(soupA, soupB)]
+
+            if soupA <= 0 and soupB > 0:
+                return 1
+            elif soupA <= 0 and soupB <= 0:
+                return 0.5
+            elif soupA > 0 and soupB <= 0:
+                return 0
+
+            ans = 0
+            for serve_A, serve_B in operations:
+                ans += 0.25 * solve(soupA - serve_A, soupB - serve_B)
+
+            memo[(soupA, soupB)] = ans  # Store the computed result in memo dict
+            return ans
+        
+        return solve(n, n)
