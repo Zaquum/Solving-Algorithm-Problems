771c8e395b2e9726420dd06ecc8c420678e31d60
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-08-02 13:25:29 +0900

    Time: 51 ms (77.73%), Space: 16.4 MB (72.51%) - LeetHub

--- LeetCode/Medium/0046-permutations/0046-permutations.py
+++ LeetCode/Medium/0046-permutations/0046-permutations.py
@@ -1,12 +1,27 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        # n = len(nums)
+        # ans = [] # answer
+        # def solve(array, remaining):
+        #     if len(array) == n:
+        #         ans.append(list(array))
+        #         return
+        #     for i in range(len(remaining)):
+        #         solve(array + [remaining[i]], remaining[:i] + remaining[i+1:])
+        # solve([], nums)
+        # return ans
+
         n = len(nums)
         ans = [] # answer
-        def solve(array, remaining):
-            if len(array) == n:
+        def solve(array, k):
+            if k == n:
                 ans.append(list(array))
                 return
-            for i in range(len(remaining)):
-                solve(array + [remaining[i]], remaining[:i] + remaining[i+1:])
-        solve([], nums)
+            for i in range(n):
+                # array.append(nums[i])
+                if nums[i] in array:
+                    continue
+                solve(array + [nums[i]], k+1)
+                # array.remove(nums[i])
+        solve([], 0)
         return ans
