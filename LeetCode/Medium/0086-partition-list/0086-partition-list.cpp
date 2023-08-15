36c4e02d576db68f6055daa0a6157b923724d7f8
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-08-15 16:26:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode less_start(0), greater_start(0); // Create dummy nodes
+        ListNode* less = &less_start;
+        ListNode* greater = &greater_start;
+
+        while (head != nullptr) {
+            if (head->val < x) {
+                less->next = head;
+                less = less->next;
+            } else {
+                greater->next = head;
+                greater = greater->next;
+            }
+            head = head->next;
+        }
+
+        less->next = greater_start.next;
+        greater->next = nullptr;
+
+        return less_start.next;
+    }
+};
