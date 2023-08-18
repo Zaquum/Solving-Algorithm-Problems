9bd256c9b016522ba48cf8dc2a6fcac721932968
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-08-18 11:02:14 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/1615-maximal-network-rank/1615-maximal-network-rank.py
+++ LeetCode/Medium/1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -5,12 +5,12 @@ class Solution:
         for road in roads:
             degrees[road[0]] += 1
             degrees[road[1]] += 1
-        
+        roads_set = set(tuple(road) for road in roads)
         max_rank = 0
         for i in range(n):
             for j in range(i+1,n):
                 rank = degrees[i] + degrees[j]
-                if [i,j] in roads or [j,i] in roads:
+                if (i,j) in roads_set or (j,i) in roads_set:
                     rank -= 1
                 max_rank = max(max_rank, rank)
         return max_rank
