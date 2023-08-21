619e70e388f8bad5c65d01f11f85994892ae038d
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-08-21 12:25:45 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ LeetCode/Easy/0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int n = s.size();
-        for(int i=n/2; i>=1 ;i--){
+        for(int i=1; i<=n/2 ;i--){
             if(n%i==0){
                 string sub = s.substr(0,i);
                 string repeated = "";
