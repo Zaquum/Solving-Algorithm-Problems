13ece3ae134c5d2aad85da3aad1cf2423896cd31
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-09-22 21:27:37 +0900

    Time: 38 ms (70.22%), Space: 16.3 MB (84.5%) - LeetHub

--- LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.py
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.py
@@ -1,7 +1,7 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         i, j = 0, 0
-        while j < len(t):
+        while j < len(t) and i < len(s):
             if s[i]==t[j]:
                 i+=1
             j+=1
