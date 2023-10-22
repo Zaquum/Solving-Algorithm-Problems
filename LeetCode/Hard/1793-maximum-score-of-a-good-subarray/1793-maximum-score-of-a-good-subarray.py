15981cb82af47de35a7938f6c3972c799b8fc61f
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-10-22 13:38:01 +0900

    Time: 1067 ms (38.78%), Space: 27.5 MB (28.06%) - LeetHub

---
+++ LeetCode/Hard/1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        left, right = k, k
+        min_val = nums[k]
+        max_score = min_val
+        while left > 0 or right < len(nums) - 1:
+            if left == 0:
+                right += 1
+            elif right == len(nums) - 1:
+                left -= 1
+            else:
+                if nums[left - 1] < nums[right + 1]:
+                    right += 1
+                else:
+                    left -= 1
+
+            min_val = min(min_val, nums[left], nums[right])
+            max_score = max(max_score, min_val * (right - left + 1))
+            
+        return max_score
