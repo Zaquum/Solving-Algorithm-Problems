74934a2de030acb6f37e213a0e4f839d678e9c47
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-10-27 09:41:13 +0900

    Time: 1849 ms (38.85%), Space: 24.3 MB (9.68%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -9,11 +9,12 @@ class Solution:
         ans = s[-1]
 
         maxLen = 1
-        for lo in range(n):
+        for lo in range(n-1, -1, -1):
             for hi in range(lo+1, n):
                 if s[lo] == s[hi]:
                     if hi - lo == 1 or dp[lo+1][hi-1]:
                         dp[lo][hi] = True
+                        # print(lo, hi)
                         if maxLen < hi - lo + 1:
                             maxLen = hi - lo + 1
                             ans = s[lo : hi+1]
