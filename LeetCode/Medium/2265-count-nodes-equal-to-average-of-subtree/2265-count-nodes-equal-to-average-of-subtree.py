3dc572cbbcef35de212411d76db6cfe526dc3158
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-11-02 12:20:38 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ LeetCode/Medium/2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -17,15 +17,18 @@ class Solution:
             return 1 + dfs_cnt(cur.left) + dfs_cnt(cur.right)
         # print(dfs_sum(root), dfs_cnt(root))
 
-        ans = []
+        # ans = []
+        result = 0
         q = deque([root])
         while q:
             cur = q.popleft()
             if dfs_sum(cur)//dfs_cnt(cur) == cur.val:
-                ans.append(cur.val)
+                # ans.append(cur.val)
+                result += 1
             if cur.left:
                 q.append(cur.left)
             if cur.right:
                 q.append(cur.right)
         # print(ans)
-        return len(ans)
+        # return len(ans)
+        return result
