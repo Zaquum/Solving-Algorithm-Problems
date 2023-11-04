68287cbc650154d7f6a83ae395b6b431b8e9c438
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2023-11-04 12:38:18 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.py
+++ LeetCode/Medium/1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -1,7 +1,5 @@
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
-        left_fall_times = [pos for pos in left]
-        
-        right_fall_times = [n - pos for pos in right]
-        
-        return max(left_fall_times + right_fall_times)
+        left_times = [pos for pos in left]
+        right_times = [n - pos for pos in right]
+        return max(left_times + right_times)
