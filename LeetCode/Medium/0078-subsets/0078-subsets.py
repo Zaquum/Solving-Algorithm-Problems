36733c0656f6533ff9412405e29af5a9b8555907
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2024-05-21 12:41:34 +0900

    Time: 35 ms (72.93%), Space: 16.7 MB (77.55%) - LeetHub

--- LeetCode/Medium/0078-subsets/0078-subsets.py
+++ LeetCode/Medium/0078-subsets/0078-subsets.py
@@ -1,14 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         self.ans = []
+        
         def dfs(idx, cur):
+            # exit case
             if idx == len(nums):
                 if cur not in self.ans:
                     self.ans.append(cur)
                 return
+            
+            # include
             dfs(idx+1, cur + [nums[idx]])
+            # exclude
             dfs(idx+1, cur)
             return
+        
         dfs(0, [])
+        
         return self.ans
             
