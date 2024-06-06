8c06c0f8ab151f9e51146e4405175be15a2ec0fb
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2024-06-06 13:19:03 +0900

    Time: 517 ms (10.25%), Space: 18.1 MB (86.00%) - LeetHub

--- LeetCode/Medium/0846-hand-of-straights/0846-hand-of-straights.py
+++ LeetCode/Medium/0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,33 +1,20 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if len(hand) % groupSize != 0:
-            return False
-        if groupSize == 1:
-            return True
+        counter = Counter(hand)
         
-        hand.sort()
-        while hand:
-            tmp = []
-            flag = False
-            while len(tmp) < groupSize and hand:
-                cur = hand[0]
-                if not tmp:
-                    cur = hand.pop(0)
-                    tmp.append(cur)
-                if tmp and tmp[-1] + 1 not in hand:
-                    flag = True
-                    return
-                elif tmp and tmp[-1] + 1 in hand:
-                    idx = 0
-                    for i in range(len(hand)):
-                        if hand[i] == tmp[-1] + 1:
-                            idx = i
-                            break
-                    cur = hand.pop(i)
-                    tmp.append(cur)
-            if flag:
-                return False
-            if len(tmp) != groupSize:
-                return False
+        # 딕셔너리를 순회하면서 그룹을 만들 수 있는지 확인
+        while counter:
+            # 가장 작은 카드 값부터 시작하여 그룹 생성
+            start = min(counter)
+            for i in range(start, start + groupSize):
+                # 만약 현재 카드 값이 딕셔너리에 없거나, 해당 카드 값의 개수가 0 이하라면 False 반환
+                if i not in counter or counter[i] <= 0:
+                    return False
+                # 그룹에서 해당 카드 값을 하나 뺍니다.
+                counter[i] -= 1
+                # 만약 그룹에서 해당 카드 값을 모두 사용했다면 딕셔너리에서 제거합니다.
+                if counter[i] == 0:
+                    del counter[i]
+        # 모든 그룹을 만들었으므로 True 반환
         return True
                 
