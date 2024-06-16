3c0cd8c6321197ce7cd6f17e956f39d3c4ff8294
YoungHun Kim <111241979+Zaquum@users.noreply.github.com>
2024-06-16 12:22:31 +0900

    Time: 45 ms (97.20%), Space: 16.6 MB (69.93%) - LeetHub

---
+++ LeetCode/Hard/0330-patching-array/0330-patching-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        # other's solution
+        miss = 1
+        result = 0
+        i = 0
+
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                result += 1
+
+        return result
